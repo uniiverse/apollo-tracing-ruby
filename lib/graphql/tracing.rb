63cee2921c47c1fd55a589e47a9282143b0fd090
Reginald <rsuh@uwaterloo.ca>
2017-08-25 14:44:50 -0400

    Return tracing information

--- lib/graphql/tracing.rb
+++ lib/graphql/tracing.rb
@@ -7,24 +7,58 @@ module GraphQL
   class Tracing
     def use(schema_definition)
       schema_definition.instrument(:query, self)
+      schema_definition.instrument(:field, self)
     end
 
     def before_query(query)
       @start_time = Time.now.utc
+      @resolvers = []
     end
 
     def after_query(query)
       result = query.result
       end_time = Time.now.utc
-      duration_nanos = ((end_time.to_f - @start_time.to_f) * 1e9).to_i
+      duration_nanos = duration_nanos(start_time: @start_time, end_time: end_time)
 
       result["extensions"] ||= {}
       result["extensions"]["tracing"] = {
         "version" => 1,
         "startTime" => @start_time.strftime('%FT%T.%3NZ'),
         "endTime" => end_time.strftime('%FT%T.%3NZ'),
-        "duration" => duration_nanos
+        "duration" => duration_nanos,
+        "execution" => {
+          "resolvers" => @resolvers
+        }
       }
+    end
+
+    def instrument(type, field)
+      old_resolve_proc = field.resolve_proc
+
+      new_resolve_proc = ->(obj, args, ctx) do
+        resolve_start_time = Time.now.utc
+        result = old_resolve_proc.call(obj, args, ctx)
+        resolve_end_time = Time.now.utc
+
+        @resolvers << {
+          'path' => ctx.path,
+          'parentType' => type.name,
+          'fieldName' => field.name,
+          'returnType' => field.type.to_s,
+          'startOffset' => duration_nanos(start_time: @start_time, end_time: resolve_start_time),
+          'duration' => duration_nanos(start_time: resolve_start_time, end_time: resolve_end_time)
+        }
+
+        result
+      end
+
+      field.redefine { resolve(new_resolve_proc) }
+    end
+
+    private
+
+    def duration_nanos(start_time:, end_time:)
+      ((end_time.to_f - start_time.to_f) * 1e9).to_i
     end
   end
 end
--- spec/graphql/tracing_spec.rb
+++ spec/graphql/tracing_spec.rb
@@ -5,12 +5,19 @@ require 'fixtures/post'
 require 'fixtures/schema'
 
 RSpec.describe Graphql::Tracing do
-  it "resolves graphql query with tracing extension" do
+  it 'returns time in RFC 3339 format' do
     query = "query($user_id: ID!) { posts(user_id: $user_id) { id title user_id } }"
     now = Time.new(2017, 8, 25)
-    allow(Time).to receive(:now).and_return(now, now + 3)
-    # allow(Time).to receive(:now).and_call_original
-    # allow(Time).to receive(:now).and_return(now + 3)
+    allow(Time).to receive(:now).and_return(now)
+
+    result = Schema.execute(query, variables: {'user_id' => "1"})
+
+    expect(result.dig("extensions", 'tracing', 'startTime')).to eq('2017-08-25T04:00:00.000Z')
+    expect(result.dig("extensions", 'tracing', 'endTime')).to eq('2017-08-25T04:00:00.000Z')
+  end
+
+  it "resolves graphql query with tracing extension" do
+    query = "query($user_id: ID!) { posts(user_id: $user_id) { id title user_id } }"
 
     result = Schema.execute(query, variables: {'user_id' => "1"})
 
@@ -21,63 +28,41 @@ RSpec.describe Graphql::Tracing do
         "user_id" => "1"
       }]
     )
-    expect(result["extensions"]).to eq(
-      "tracing" => {
-        "version" => 1,
-        "startTime" => "2017-08-25T04:00:00.000Z",
-        "endTime" => "2017-08-25T04:00:03.000Z",
-        "duration" => 3_000_000_000,
-        # "execution" => {
-        #   "resolvers" => [
-        #     {
-        #       "path" => [
-        #         "posts"
-        #       ],
-        #       "parentType" => "Query",
-        #       "fieldName" => "posts",
-        #       "returnType" => "[Post!]!",
-        #       "startOffset" => 1172456,
-        #       "duration" => 215657
-        #     },
-        #     {
-        #       "path" => [
-        #         "posts",
-        #         0,
-        #         "id"
-        #       ],
-        #       "parentType" => "Query",
-        #       "fieldName" => "id",
-        #       "returnType" => "ID!",
-        #       "startOffset" => 1903307,
-        #       "duration" => 73098
-        #     },
-        #     {
-        #       "path" => [
-        #         "posts",
-        #         0,
-        #         "title"
-        #       ],
-        #       "parentType" => "Query",
-        #       "fieldName" => "title",
-        #       "returnType" => "String!",
-        #       "startOffset" => 1903307,
-        #       "duration" => 73098
-        #     },
-        #     {
-        #       "path" => [
-        #         "posts",
-        #         0,
-        #         "user_id"
-        #       ],
-        #       "parentType" => "Query",
-        #       "fieldName" => "user_id",
-        #       "returnType" => "ID!",
-        #       "startOffset" => 1903307,
-        #       "duration" => 73098
-        #     }
-        #   ]
-        # }
-      }
-    )
+    tracing = result.dig("extensions", 'tracing')
+
+    expect(tracing['version']).to eq(1)
+    expect(tracing['startTime']).to be_a(String)
+    expect(tracing['endTime']).to be_a(String)
+    expect(tracing['duration']).to be >= 0
+
+    resolvers = tracing.dig('execution', 'resolvers')
+
+    expect(resolvers.dig(0, 'path')).to eq(["posts"])
+    expect(resolvers.dig(0, 'parentType')).to eq("Query")
+    expect(resolvers.dig(0, 'fieldName')).to eq("posts")
+    expect(resolvers.dig(0, 'returnType')).to eq("[Post!]!")
+    expect(resolvers.dig(0, 'startOffset')).to be >= 0
+    expect(resolvers.dig(0, 'duration')).to be >= 0
+
+    expect(resolvers.dig(1, 'path')).to eq(["posts", 0, "id"])
+    expect(resolvers.dig(1, 'parentType')).to eq("Post")
+    expect(resolvers.dig(1, 'fieldName')).to eq("id")
+    expect(resolvers.dig(1, 'returnType')).to eq("ID!")
+    expect(resolvers.dig(1, 'startOffset')).to be >= 0
+    expect(resolvers.dig(1, 'duration')).to be >= 0
+
+    expect(resolvers.dig(2, 'path')).to eq(["posts", 0, "title"])
+    expect(resolvers.dig(2, 'parentType')).to eq("Post")
+    expect(resolvers.dig(2, 'fieldName')).to eq("title")
+    expect(resolvers.dig(2, 'returnType')).to eq("String!")
+    expect(resolvers.dig(2, 'startOffset')).to be >= 0
+    expect(resolvers.dig(2, 'duration')).to be >= 0
+
+    expect(resolvers.dig(3, 'path')).to eq(["posts", 0, "user_id"])
+    expect(resolvers.dig(3, 'parentType')).to eq("Post")
+    expect(resolvers.dig(3, 'fieldName')).to eq("user_id")
+    expect(resolvers.dig(3, 'returnType')).to eq("ID!")
+    expect(resolvers.dig(3, 'startOffset')).to be >= 0
+    expect(resolvers.dig(3, 'duration')).to be >= 0
   end
 end
